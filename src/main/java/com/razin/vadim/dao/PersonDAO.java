563d79fd227ad42be054d84d4465167684c2db6f
Vadim Razin <vadim.razin@gmail.com>
2022-12-17 14:38:38 +0600

    finish lesson 30 Пакетное обновление (Batch update)

--- src/main/java/com/razin/vadim/dao/PersonDAO.java
+++ src/main/java/com/razin/vadim/dao/PersonDAO.java
@@ -1,8 +1,8 @@
 package com.razin.vadim.dao;
 
-import com.razin.vadim.controllers.PersonMapper;
 import com.razin.vadim.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -18,7 +18,7 @@ public class PersonDAO {
 
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate=jdbcTemplate;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Person> index() {
@@ -44,5 +44,59 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES (?, ?, ?, ?)", person.getId(), person.getName(),
+                    person.getAge(), person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+
+    }
+
+
+    public void testBatchUpdate() {
+        List<Person> people = create1000People();
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+           // используем для ставки метод batchUpdate, 1-ый параметр также SQL запрос, 2-ой параметр
+            // new BatchPreparedStatementSetter() - и здесь же реализуем анонимный класс
+            jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?,?,?,?)",
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                            // должны указать объекты для записи
+                            preparedStatement.setInt(1, people.get(i).getId());
+                            preparedStatement.setString(2, people.get(i).getName());
+                            preparedStatement.setInt(3, people.get(i).getAge());
+                            preparedStatement.setString(4, people.get(i).getEmail());
+                        }
+
+                        @Override
+                        public int getBatchSize() {
+                            // должны указать размер пакета
+                            return people.size();
+                        }
+                    });
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add((new Person(i, "Name" + i, 30, "test" + i + "@mail.ru")));
+        }
+        return people;
     }
 }
